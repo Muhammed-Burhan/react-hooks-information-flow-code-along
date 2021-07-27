39bd6f4d014e7896b1f84d6d4bc1739eb18c3019
Muhammed <muhammadburhan964@gmail.com>
2021-07-28 02:49:47 +0300

    commit tree 4af2d7481b291e8cbb73d1afded3426211b0ed79

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
-
+import { getRandomColor } from "./randomColorGenerator";
 function Child(props) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+  //my way
+  // const handleChangeColor = () => {
+  //   setColor((prev) => (prev = random));
+  // };
+  // const [childColor, setChildColor] = useState();
+  return (
+    <div
+      onClick={() => props.onChangeColor(getRandomColor())}
+      className="child"
+      style={{ backgroundColor: props.color }}
+    />
+  );
 }
 
 export default Child;
